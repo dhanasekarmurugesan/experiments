1ced786de86c9af31b26c4c755778b61d6005cbe
Dhanasekar Murugesan <11454416+dhanasekarmurugesan@users.noreply.github.com>
2022-03-25 19:22:03 +0530

    files uploaded now.

---
+++ LibraryExperiment/LibraryExperiment/Program.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = InitializeAutomapper();
+            IMapper mapper = config.CreateMapper();
+
+            ClassA classA = new ClassA { Value = "B" };
+            ClassB classB = mapper.Map<ClassB>(classA);
+
+
+
+            Console.ReadLine();
+        }
+
+        static MapperConfiguration InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<ClassA, ClassB>()
+               .ForMember(b => b.Value, a => a.ConvertUsing<string>(new StringToEnumConverter<EnumTypes>(), y => y.Value));
+
+            });
+            return config;
+        }
+    }
+
+    public class ClassA
+    {
+        public string Value { get; set; }
+    }
+
+    public class ClassB
+    {
+        public EnumTypes Value { get; set; }
+    }
+
+
+    public enum EnumTypes: short
+    {
+        A = 0,
+        B = 1
+    }
+}
---
+++ LibraryExperiment/LibraryExperiment/StringToEnumConverter.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+
+namespace LibraryExperiment
+{
+    internal class StringToEnumConverter<T> : IValueConverter<string, T> where T: struct
+    {
+        public T Convert(string sourceMember, ResolutionContext context)
+        {
+            if(Enum.TryParse(sourceMember, out T resolvedValue))
+            {
+                return resolvedValue;
+            }
+
+            return default(T);
+        }
+
+
+    }
+}
